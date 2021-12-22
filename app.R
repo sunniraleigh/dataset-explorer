d69d01f951bc35f17ddd0cff099149b902015689
sunniraleigh <sraleigh@smith.edu>
2021-12-22 18:44:37 -0500

    initial commit

---
+++ app.R
@@ -0,0 +1,16 @@
+# Load Packages
+library(shiny)
+library(tidyverse)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
