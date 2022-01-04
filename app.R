f9b1c511c5b02daa31b568d59289a00036bf9f11
sunniraleigh <sraleigh@smith.edu>
2022-01-04 13:12:11 -0500

    add file upload

--- app.R
+++ app.R
@@ -1,17 +1,43 @@
-# Load Packages
+# LOAD PACKAGES
 library(shiny)
+library(shinydashboard)
 library(tidyverse)
 library(palmerpenguins)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# FUNCTIONS
 
+
+# UI
+ui <- fluidPage(
+  dashboardHeader(),
+  dashboardSidebar(
+    fileInput("upload", NULL, buttonLabel = "Upload", multiple = FALSE, accept = ".csv")
+  ),
+  dashboardBody(
+    tableOutput("head")
+  )
 )
 
-# Define server logic required to draw a histogram
+# SERVER
 server <- function(input, output) {
-
+  # Handle data upload
+  dataset <- shiny::reactice({
+    req(input$upload)
+    
+    # validate input
+    ext <- tools::file_ext(input$upload$name)
+    switch(ext,
+           csv = vroom::vroom(input$upload$datapath, delim = ","),
+           validate("Invalid file; Please upload a .csv file")
+    )
+  })
+  
+  # Create ouput
+  output$head <- renderTable({
+    head(data())
+  })
+  
 }
 
-# Run the application 
+# RUN
 shinyApp(ui = ui, server = server)
